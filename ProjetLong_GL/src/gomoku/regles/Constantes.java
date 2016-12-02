69d4329eb0e83bd5e663c4fc1ed9c04270c6bc2f
markeyj <markeyj@meleze15.iut-infobio.priv.univ-lille1.fr>
2016-12-02 10:02:44 +0100

    Modif plain de truc

--- ProjetLong_GL/src/gomoku/regles/Constantes.java
+++ ProjetLong_GL/src/gomoku/regles/Constantes.java
@@ -1,23 +1,15 @@
 package gomoku.regles;
 
-public class Constantes {
+public class Constantes 
+{
 	/** constante qui va définir la taille de la grille */
-	public final static int SIZE = 10;
+	public final static int SIZE = 19;
 
 	/** hauteur du plateau */
-	public final static int HP = SIZE*50;
+	public final static int HP = SIZE*40;
 	
 	/** largeur du plateau */
-	public final static int LP = SIZE*50;
-	
-	/** constante pour symboliser le joueur2 */
-    public static int JOUEUR_ACTUEL = 1;
-    
-    /** constante pour symboliser le joueur1 */
-    public final static int JOUEUR1 = 1;
-    
-    /** constante pour symboliser le joueur2 */
-    public final static int JOUEUR2 = 2;
+	public final static int LP = SIZE*40;
 	
 	/** constante pour symboliser le joueur humain */
     public final static int HUMAN = 0;
@@ -27,5 +19,8 @@ public class Constantes {
 	
     /** constante pour symboliser une case sans pions */
     public final static String CASE_INACTIVE = ".";
+    
+    /** Permet de préciser si les jeton doivent ếtre obligatoirement placé à coté d'un autre jeton */
+    public static boolean aCote = true;
     
 }
